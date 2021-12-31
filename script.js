e982f0577f673fd36d74f3c4207a54dd150f955b
Jian Q Liang <30308734+jqliang14@users.noreply.github.com>
2021-12-31 17:04:12 -0600

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,12 @@
+// let time = 0;
+
+// while (time < 10) {
+//     console.log('tried it', time);
+//     time++;
+// }
+
+var myList = ['Apple', 'Orange', 'Banana'];
+for (var i= 0; i < myList.length; i++) {
+    alert('you have '+myList[1]+' in your basket');
+    // console.log(myList[1]);
+}
